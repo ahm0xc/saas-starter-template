060bad8abefffbc0232719c13bc14e7598495d0f
ahm0xc <codingwebsa@gmail.com>
2024-06-12 21:51:54 +0600

    mock dashboard page

--- apps/www/src/app/(landing)/_components/nav-action-buttons.tsx
+++ apps/www/src/app/(landing)/_components/nav-action-buttons.tsx
@@ -1,24 +1,38 @@
 import React from "react";
+import Link from "next/link";
 import { ArrowRightIcon } from "@iconicicons/react";
 import { Button } from "@lemonsqueezy/wedges";
-import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function NavActionButtons() {
   return (
     <div className="flex items-center gap-4">
-      <Link
-        href="/sign-in"
-        className="text-white/70 duration-200 hover:text-white"
-      >
-        Sign in
-      </Link>
-      <Button
-        after={<ArrowRightIcon />}
-        variant="tertiary"
-        className="rounded-full"
-      >
-        Get Started
-      </Button>
+      <SignedIn>
+        <Button
+          asChild
+          after={<ArrowRightIcon />}
+          variant="tertiary"
+          className="rounded-full"
+        >
+          <Link href="/dashboard">Dashboard</Link>
+        </Button>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <Link
+          href="/sign-in"
+          className="text-white/70 duration-200 hover:text-white"
+        >
+          Sign in
+        </Link>
+        <Button
+          after={<ArrowRightIcon />}
+          variant="tertiary"
+          className="rounded-full"
+        >
+          Get Started
+        </Button>
+      </SignedOut>
     </div>
   );
 }
